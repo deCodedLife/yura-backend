e1be266b21934ed297055837ed1f7a523e52e5d3
deCodedLife <dcodedlife@gmail.com>
2022-12-06 22:50:53 +0300

    CORS update

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Headers:", "*")
+		w.Header().Set("Access-Control-Allow-Headers:", "Origin, X-Requested-With, Content-Type, Accept")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
 
