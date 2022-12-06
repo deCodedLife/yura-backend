4649270d4ecdcf7558fa6383fd52a67064f084a6
deCodedLife <dcodedlife@gmail.com>
2022-12-06 12:31:14 +0300

    Remake file uploads

--- api/images/api.go
+++ api/images/api.go
@@ -2,43 +2,60 @@ package api
 
 import (
 	"crypto/sha512"
+	"errors"
 	"fmt"
 	. "github.com/deCodedLife/gorest/tool"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
-	"unicode/utf8"
+	"time"
 )
 
-func splitFileName(f string) (string, string) {
-	namePieces := strings.Split(f, ".")
-	extension := namePieces[len(namePieces)]
-	extensionLength := utf8.RuneCountInString(extension)
-	fileName := f[:len(namePieces)-extensionLength]
-	return fileName, extension
-}
-
 func UploadImage(w http.ResponseWriter, r *http.Request) {
-	file, handler, err := r.FormFile("image")
+
+	err := r.ParseMultipartForm(5 << 20)
 	HandleError(err, CustomError{}.WebError(w, http.StatusNotAcceptable, err))
 
-	defer file.Close()
+	files := r.MultipartForm.File["image"]
+	var output []string
 
-	fileName, extension := splitFileName(handler.Filename)
+	for _, handler := range files {
 
-	chipper := sha512.New()
-	chipper.Write([]byte(fileName))
+		file, err := handler.Open()
+		HandleError(err, CustomError{}.Unexpected(err))
 
-	outputFileName := fmt.Sprintf("%x.%s", chipper.Sum(nil), extension)
+		defer file.Close()
 
-	imageFile, err := os.OpenFile(outputFileName, os.O_WRONLY|os.O_CREATE, 0666)
-	HandleError(err, CustomError{}.Unexpected(err))
+		buff := make([]byte, 512)
+		_, err = file.Read(buff)
+		HandleError(err, CustomError{}.Unexpected(err))
 
-	defer imageFile.Close()
+		filetype := http.DetectContentType(buff)
+		if len(strings.Split(filetype, "image")) == 1 {
+			err := errors.New("only images expected")
+			HandleError(err, CustomError{}.WebError(w, http.StatusNotAcceptable, err))
+		}
 
-	_, err = io.Copy(imageFile, file)
-	HandleError(err, CustomError{}.Unexpected(err))
+		_, err = file.Seek(0, io.SeekStart)
+		HandleError(err, CustomError{}.Unexpected(err))
 
-	SendData(w, 200, outputFileName)
+		chipper := sha512.New()
+		chipper.Write([]byte(fmt.Sprintf("%d_%d", time.Now().UnixNano(), filepath.Base(handler.Filename))))
+
+		outputFileName := fmt.Sprintf("%x%s", chipper.Sum(nil), filepath.Ext(handler.Filename))
+
+		serverFile, err := os.Create(fmt.Sprintf("./assets/%s", outputFileName))
+		defer serverFile.Close()
+
+		_, err = io.Copy(serverFile, file)
+		HandleError(err, CustomError{}.Unexpected(err))
+
+		output = append(output, outputFileName)
+
+	}
+
+	SendData(w, 200, output)
+
 }
