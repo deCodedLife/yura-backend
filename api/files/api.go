3630cce9088f5a87f36901f008f9ea1cfa6e048a
CodedLife <dcodedlife@gmail.com>
2023-03-03 19:28:12 +0300

    Making a file server #Listing files added

--- api/files/api.go
+++ api/files/api.go
@@ -13,11 +13,14 @@ type FilesRequest struct {
 	Path string `json:"path"`
 }
 
-func LS(w http.ResponseWriter, r http.Request) {
+func LS(w http.ResponseWriter, r *http.Request) {
 	var request FilesRequest
 
 	err := json.NewDecoder(r.Body).Decode(&request)
 	HandleError(err, CustomError{}.WebError(w, http.StatusNotAcceptable, err))
 
-	SendData(w, http.StatusOK, interface{}(ListDirectory(UPLOADS_FOLDER+request.Path)))
+	folderContent, err := ListDirectory(UPLOADS_FOLDER + request.Path)
+	HandleError(err, CustomError{}.WebError(w, http.StatusInternalServerError, err))
+
+	SendData(w, http.StatusOK, folderContent)
 }
--- api/handlers.go
+++ api/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	. "backend/api/exel"
+	. "backend/api/files"
 	. "backend/api/images"
 	. "backend/api/users"
 )
@@ -13,4 +14,5 @@ func InitRouters(r *mux.Router) {
 	r.HandleFunc("/api/sign-in", SignIn).Methods(http.MethodPost)
 	r.HandleFunc("/api/images", UploadImage).Methods(http.MethodPost)
 	r.HandleFunc("/api/exel", UploadTables).Methods(http.MethodPost)
+	r.HandleFunc("/api/fm/ls", LS).Methods(http.MethodPost)
 }
