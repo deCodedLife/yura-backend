20250e5bbe67baf652dd3969bf20d6b367b3ee73
CodedLife <dcodedlife@gmail.com>
2023-03-18 01:41:02 +0300

    Handle all uploading files

--- api/files/api.go
+++ api/files/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"errors"
+	"mime/multipart"
 	"net/http"
 
 	. "github.com/deCodedLife/gorest/tool"
@@ -64,15 +65,26 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	HandleError(err, CustomError{}.WebError(w, http.StatusNotAcceptable, err))
 
 	files := r.MultipartForm.File["file"]
+	var filesList []string
 
-	filesList, err := HandleFile(files, FileConfigs{
-		FileType:    "",
-		FileSubType: nil,
-		TypeError:   errors.New("something went wrong"),
-		SavePath:    r.MultipartForm.Value["path"][0],
-	})
+	for index, file := range files {
 
-	HandleError(err, CustomError{}.WebError(w, http.StatusNotAcceptable, err))
+		var eachFile []*multipart.FileHeader
+
+		eachFile = append(eachFile, file)
+
+		filePath, err := HandleFile(eachFile, FileConfigs{
+			FileType:    "",
+			FileSubType: nil,
+			TypeError:   errors.New("something went wrong"),
+			SavePath:    r.MultipartForm.Value["path"][index],
+		})
+
+		HandleError(err, CustomError{}.WebError(w, http.StatusNotAcceptable, err))
+		filesList = append(filesList, filePath[0])
+
+	}
+
 	SendData(w, http.StatusOK, filesList)
 
 }
